47676c99ea5f3c45a244a845b0ec250b4d440eec
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2020-02-10 19:11:07 +0000

    Daily Report class

---
+++ app/src/main/java/com/tt/reptool/DailyReport.java
@@ -0,0 +1,24 @@
+package com.tt.reptool;
+
+/*
+
+Daily report - what kind of job and where was taken
+
+ - date
+ - time in and out
+ - job
+ - description (what has been done)
+ - any additional info for management
+ - any accidents
+
+ */
+
+
+import java.util.Date;
+
+public class DailyReport {
+
+    private Date date;
+    // TODO finish this class first!!!
+
+}
