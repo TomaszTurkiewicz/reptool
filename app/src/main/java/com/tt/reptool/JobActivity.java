614e65c9183478d1ba04993ee78ceb9e4e4deae4
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2020-02-22 18:15:50 +0000

    working spinner and adding job to database

--- app/src/main/java/com/tt/reptool/JobActivity.java
+++ app/src/main/java/com/tt/reptool/JobActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -30,6 +32,7 @@ public class JobActivity extends AppCompatActivity {
     private DatabaseReference databaseReferenceJob;
     private DatabaseReference databaseReferenceManager;
     private List<Manager> mList = new ArrayList<>();
+    private ManagerSpinnerAdapter managerSpinnerAdapter;
     private Manager pm = new Manager();
     Job job = new Job();
 
@@ -42,30 +45,51 @@ public class JobActivity extends AppCompatActivity {
         jobStreet = findViewById(R.id.street);
         jobPostcode = findViewById(R.id.postcode);
         jobDescription = findViewById(R.id.jobDescription);
-        jobPMSpinner = (Spinner)findViewById(R.id.jobPM);
+
+
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReferenceJob=firebaseDatabase.getReference("Job");
         databaseReferenceManager = firebaseDatabase.getReference("Manager");
-        initManagerList();
-        ManagerSpinnerAdapter managerSpinnerAdapter = new ManagerSpinnerAdapter(this,mList);
-        jobPMSpinner.setAdapter(managerSpinnerAdapter);
 
-
-        jobPMSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        mList.clear();
+        databaseReferenceManager.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Manager clickedItem = (Manager) parent.getItemAtPosition(position);
-                pm.setName(clickedItem.getName());
-                pm.setSurname(clickedItem.getSurname());
-                pm.setEmailAddress(clickedItem.getEmailAddress());
-                job.setProjectManager(pm);
+
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ps : dataSnapshot.getChildren()){
+                    Manager manager = ps.getValue(Manager.class);
+                    mList.add(manager);
+                }
+                jobPMSpinner = (Spinner)findViewById(R.id.jobPM);
+                managerSpinnerAdapter = new ManagerSpinnerAdapter(JobActivity.this,mList);
+                jobPMSpinner.setAdapter(managerSpinnerAdapter);
+                jobPMSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        Manager clickedItem = (Manager) parent.getItemAtPosition(position);
+                        pm.setName(clickedItem.getName());
+                        pm.setSurname(clickedItem.getSurname());
+                        pm.setEmailAddress(clickedItem.getEmailAddress());
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
+
+
+
+
     }
 
 
@@ -85,6 +109,7 @@ public class JobActivity extends AppCompatActivity {
         job.setJobNumber(jNumber);
         job.setAddress(jAddress);
         job.setShortDescription(jDescription);
+        job.setProjectManager(pm);
 
         databaseReferenceJob.child(jNumber).setValue(job);
         jobNumber.setText("");
@@ -95,27 +120,6 @@ public class JobActivity extends AppCompatActivity {
 
     }
 
-    private void initManagerList() {
-
-        mList.clear();
-        databaseReferenceManager.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot ps : dataSnapshot.getChildren()){
-                    Manager manager = ps.getValue(Manager.class);
-                    mList.add(manager);
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-    }
 }
 //TODO why doesn't add manager to job?
 //TODO empty fields job
